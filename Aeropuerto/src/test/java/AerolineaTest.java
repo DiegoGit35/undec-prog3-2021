0a740d49a41c05fbfb24e55100c9e970641d3c4e
Estrada <diegoestrada35@gmail.com>
2021-04-05 13:12:46 -0300

    simplificación de tests

--- Aeropuerto/src/test/java/AerolineaTest.java
+++ Aeropuerto/src/test/java/AerolineaTest.java
@@ -9,87 +9,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AerolineaTest {
     GregorianCalendar f1 = new GregorianCalendar(1996, 11, 27);
+    GregorianCalendar f2 = new GregorianCalendar(1983, 04, 13);
+    GregorianCalendar f3 = new GregorianCalendar(2000, 06, 06);
     Aerolinea aerolinea = new Aerolinea("30-64140555-4", "ar", "Aerolíneas Argentinas", f1);
+    Aerolinea aerolinea2 = new Aerolinea("30-64142124-2", "CL", "Aerolíneas Argentinas", f2);
+    Aerolinea aerolinea3 = new Aerolinea("30-64140515-4", "UR", "Aerolíneas Argentinas", f3);
 
+//  Cargar una aerolínea nueva
     @Test
-    void test01getCuit() throws ExceptionAerolinea {
-        assertEquals("30-64140555-4", aerolinea.getCuit());
+    void test01addAerolinea() throws ExceptionAerolinea {
+        GestorAerolieas gestorAerolinea = new GestorAerolieas();
+        assertEquals(0, gestorAerolinea.cantAerolineas());
+        gestorAerolinea.addAerolinea(aerolinea);
+        assertEquals(1, gestorAerolinea.cantAerolineas());
+        gestorAerolinea.addAerolinea(aerolinea);
+        gestorAerolinea.addAerolinea(aerolinea2);
+        assertEquals(2, gestorAerolinea.cantAerolineas());
     }
 
+//  Buscar aerolíneas por nombre
     @Test
-    void test02getCuit() throws ExceptionAerolinea {
-        Aerolinea aerolinea = new Aerolinea(null, "ar", "Aerolíneas Argentinas", f1);
-    }
-
-    @Test
-    void test03setCuit() throws ExceptionAerolinea {
-        aerolinea.setCuit("30-64140555-3");
-        assertEquals("30-64140555-3", aerolinea.getCuit());
-    }
-
-    @Test
-    void test04getCodIATA() throws ExceptionAerolinea {
-        assertEquals("AR", aerolinea.getCodIATA());
-    }
-
-    @Test
-    void test06setCodIATA() throws ExceptionAerolinea {
-        assertEquals("AS", aerolinea.getCodIATA());
-    }
-
-    @Test
-    void test07getNombre() throws ExceptionAerolinea {
-        assertEquals("Aerolíneas Argentinas", aerolinea.getNombre());
-    }
-
-    @Test
-    void test08setNombre() throws ExceptionAerolinea {
-        aerolinea.setNombre("Alas del Sur");
-        assertEquals("Alas Del Sur", aerolinea.getNombre());
-    }
-
-    @Test
-    void test09getFecha() throws ExceptionAerolinea {
-        assertEquals("27/11/1996", aerolinea.getFecha());
-    }
-
-    @Test
-    void test10SetFecha() throws ExceptionAerolinea {
-        aerolinea.setFecha(2019, 9, 11);
-        assertEquals("11/10/2019", aerolinea.getFecha());
-    }
-
-//    12. Cargar una aerolínea nueva
-    @Test
-    void test11addAerolinea() throws ExceptionAerolinea {
-        GregorianCalendar f1 = new GregorianCalendar(1996, 11, 27);
-        GregorianCalendar f2 = new GregorianCalendar(1983, 04, 13);
-        Aerolinea aerolinea = new Aerolinea("30-64140555-4", "ar", "Aerolíneas Argentinas", f1);
-        Aerolinea aerolinea2 = new Aerolinea("30-64142124-2", "CL", "Aerolíneas Argentinas", f2);
-        GestorAerolieas ga = new GestorAerolieas();
-        assertEquals(0, ga.cantAerolineas());
-        ga.addAerolinea(aerolinea);
-        assertEquals(1, ga.cantAerolineas());
-        ga.addAerolinea(aerolinea);
-        ga.addAerolinea(aerolinea2);
-        assertEquals(2, ga.cantAerolineas());
-    }
-
-//    14. Buscar aerolíneas por nombre
-    @Test
-    void test12buscarAerolineaxNombre() throws ExceptionAerolinea {
-        GregorianCalendar f1 = new GregorianCalendar(1996, 11, 27);
-        GregorianCalendar f2 = new GregorianCalendar(1983, 04, 13);
-        GregorianCalendar f3 = new GregorianCalendar(2000, 06, 06);
-        Aerolinea aerolinea = new Aerolinea("30-64140555-4", "ar", "Aerolíneas Argentinas", f1);
-        Aerolinea aerolinea2 = new Aerolinea("30-64142124-2", "CL", "Aerolíneas Argentinas", f2);
-        Aerolinea aerolinea3 = new Aerolinea("30-64140515-4", "UR", "Aerolíneas Argentinas", f3);
-        GestorAerolieas ga = new GestorAerolieas();
-        ga.addAerolinea(aerolinea);
-        ga.addAerolinea(aerolinea2);
-        ga.addAerolinea(aerolinea3);
+    void test02buscarAerolineaxNombre() throws ExceptionAerolinea {
+        GestorAerolieas gestorAerolinea = new GestorAerolieas();
+        gestorAerolinea.addAerolinea(aerolinea);
+        gestorAerolinea.addAerolinea(aerolinea2);
+        gestorAerolinea.addAerolinea(aerolinea3);
         List<Aerolinea> lista2 = new ArrayList<Aerolinea>();
         lista2.add(aerolinea);
-        assertEquals(lista2, ga.buscarAerolineaXnombre("Aerolíneas Argentinas"));
+        assertEquals(lista2, gestorAerolinea.buscarAerolineaXnombre("Aerolíneas Argentinas"));
     }
+//  Modificar aerolinea
 }
--- Aeropuerto/src/test/java/AeropuertoTest.java
+++ Aeropuerto/src/test/java/AeropuertoTest.java
@@ -11,82 +11,42 @@ class AeropuertoTest {
     Aeropuerto a2 = new Aeropuerto("ALP", "Aeropuerto La Plata", "La Plata", 5132);
     Aeropuerto a3 = new Aeropuerto("ALR", "Aeropuerto La Rioja", "La Rioja", 6421);
 
-    AeropuertoTest() throws ExceptionAeropuerto {
-    }
 
+//  Cargar un aeropuerto nuevo
     @Test
-    void test01getCodIATA() {
-        assertEquals("EZE", a.getCodIATA());
+    void test01addAeropuerto(){
+        GestorAeropuerto gestorAeropuerto = new GestorAeropuerto();
+        gestorAeropuerto.addAeropuerto(a);
+        gestorAeropuerto.addAeropuerto(a2);
+        gestorAeropuerto.addAeropuerto(a3);
+        gestorAeropuerto.addAeropuerto(a3);
+        assertEquals(3, gestorAeropuerto.cantAeropuertos());
     }
-
+//  Buscar aeropuerto por código IATA
     @Test
-    void test02setCodIATA() {
-        a.setCodIATA("ACH");
-        assertEquals("ACH", a.getCodIATA());
-    }
-
-    @Test
-    void test03getNombre() {
-        assertEquals("Ezeiza", a.getNombre());
-    }
-
-    @Test
-    void test04setNombre() throws ExceptionAeropuerto {
-        a.setNombre("Aeropuerto la rioja");
-        assertEquals("Aeropuerto La Rioja", a.getNombre());
-    }
-
-    @Test
-    void test05getCiudad() {
-        assertEquals("Chilecito", a.getCiudad());
-    }
-
-    @Test
-    void test06setCiudad() {
-        a.setCiudad("famatina");
-        assertEquals("Famatina", a.getCiudad());
-    }
-
-    @Test
-    void test07getCodPostal() {
-        assertEquals(5360, a.getCodPostal());
-    }
-
-//    15. Cargar un aeropuerto nuevo
-    @Test
-    void test08addAeropuerto(){
-        GestorAeropuerto gap = new GestorAeropuerto();
-        gap.addAeropuerto(a);
-        gap.addAeropuerto(a2);
-        gap.addAeropuerto(a3);
-        gap.addAeropuerto(a3);
-        assertEquals(3, gap.cantAeropuertos());
-    }
-//    16. Modificar un aeropuerto existente
-
-//    17. Consultar los aeropuertos
-    @Test
-    void test09getAeropuertos(){
+    void test02buscarAeropuertosXcodIATA(){
         List<Aeropuerto> lista2 = new ArrayList<Aeropuerto>();
-        GestorAeropuerto gap = new GestorAeropuerto();
-        gap.addAeropuerto(a);
-        gap.addAeropuerto(a2);
-        gap.addAeropuerto(a3);
+        GestorAeropuerto gestorAeropuerto = new GestorAeropuerto();
+        gestorAeropuerto.addAeropuerto(a);
+        gestorAeropuerto.addAeropuerto(a2);
+        gestorAeropuerto.addAeropuerto(a3);
+        lista2.add(a2);
+        assertEquals(lista2, gestorAeropuerto.buscarXcodIATA("ALP"));
+    }
+
+//  Consultar los aeropuertos
+    @Test
+    void test03getAeropuertos(){
+        List<Aeropuerto> lista2 = new ArrayList<Aeropuerto>();
+        GestorAeropuerto gestorAeropuerto = new GestorAeropuerto();
+        gestorAeropuerto.addAeropuerto(a);
+        gestorAeropuerto.addAeropuerto(a2);
+        gestorAeropuerto.addAeropuerto(a3);
         lista2.add(a);
         lista2.add(a2);
         lista2.add(a3);
-        assertEquals(lista2, gap.getAeropuertos());
+        assertEquals(lista2, gestorAeropuerto.getAeropuertos());
     }
-//    18. Buscar aeropuerto por código IATA
-    @Test
-    void test10buscarAeropuertosXcodIATA(){
-        List<Aeropuerto> lista2 = new ArrayList<Aeropuerto>();
-        GestorAeropuerto gap = new GestorAeropuerto();
-        gap.addAeropuerto(a);
-        gap.addAeropuerto(a2);
-        gap.addAeropuerto(a3);
-        lista2.add(a2);
-        assertEquals(lista2, gap.buscarXcodIATA("ALP"));
-    }
+//  Modificar un aeropuerto existente
 
 }
